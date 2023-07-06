53e3f3e65fa8416a2c3610f109bdd8e7b60c2856
Laughing-q <1185102784@qq.com>
2023-07-06 10:14:18 +0800

    udpate

--- rename.py
+++ rename.py
@@ -6,26 +6,26 @@ import glob
 import os.path as osp
 
 
-dir_name = "RD"
-video_dir = f"/d/dataset/audio/HDTF_DATA/{dir_name}25_images"
-video_files = glob.glob(osp.join(video_dir, "*"))
-video_names = [Path(v).with_suffix("").name for v in video_files]
-
-with open(f"HDTF_dataset/{dir_name}_video_url.txt", 'r') as f:
-    urls = [l.strip().split() for l in f.readlines()]
-
-print(f"Local files:{len(video_files)}\nOfficial files:{len(urls)}")
-for name, url in urls:
-    local_name = url.split("?")[-1][2:]
-    print(name, local_name, local_name in video_names)
-    if not (local_name in video_names):
-        continue
-    idx = video_names.index(local_name)
-    # print(idx)
-    p = Path(video_files[idx])
-    # print(p.suffix)
-    new_name = Path(video_dir) / f"{name}{p.suffix}"
-    Path(video_files[idx]).rename(new_name)
+# dir_name = "RD"
+# video_dir = f"/d/dataset/audio/HDTF_DATA/{dir_name}25_images"
+# video_files = glob.glob(osp.join(video_dir, "*"))
+# video_names = [Path(v).with_suffix("").name for v in video_files]
+#
+# with open(f"HDTF_dataset/{dir_name}_video_url.txt", 'r') as f:
+#     urls = [l.strip().split() for l in f.readlines()]
+#
+# print(f"Local files:{len(video_files)}\nOfficial files:{len(urls)}")
+# for name, url in urls:
+#     local_name = url.split("?")[-1][2:]
+#     print(name, local_name, local_name in video_names)
+#     if not (local_name in video_names):
+#         continue
+#     idx = video_names.index(local_name)
+#     # print(idx)
+#     p = Path(video_files[idx])
+#     # print(p.suffix)
+#     new_name = Path(video_dir) / f"{name}{p.suffix}"
+#     Path(video_files[idx]).rename(new_name)
     # print(new_name)
 
 # NOTE: Clip videos
@@ -59,12 +59,12 @@ for name, url in urls:
 #         )
 
 # NOTE: udpate the name of images.
-# im_dirs = glob.glob("/d/dataset/audio/HDTF_DATA/RD25_images/*")
-# for im_dir in tqdm(im_dirs, total=len(im_dirs)):
-#     im_files = sorted(glob.glob(osp.join(im_dir, "*")), key=lambda x: int(Path(x).with_suffix("").name), reverse=True)
-#     for im_file in im_files:
-#         p = Path(im_file)
-#         suffix = p.suffix
-#         name = int(p.with_suffix("").name)
-#         new_name = p.parent / f"{name + 1}{suffix}"
-#         p.rename(new_name)
+im_dirs = glob.glob("/d/dataset/audio/CMLRdataset/images/s5/*")
+for im_dir in tqdm(im_dirs, total=len(im_dirs)):
+    im_files = sorted(glob.glob(osp.join(im_dir, "*")), key=lambda x: int(Path(x).with_suffix("").name))
+    for im_file in im_files:
+        p = Path(im_file)
+        suffix = p.suffix
+        name = int(p.with_suffix("").name)
+        new_name = p.parent / f"{name - 1}{suffix}"
+        p.rename(new_name)
